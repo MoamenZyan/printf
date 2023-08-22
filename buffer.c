dd39c6dde6bb26bbe1ce04914b9cbadd5d57c264
Bassma <bm105257@gmail.com>
2023-08-22 12:45:26 +0300

    added

--- buffer.c
+++ buffer.c
@@ -1,14 +1,17 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
-static int i = 0;
+int i = 0;
+int len = 0;
 
 void add_to_buffer_string(char buffer[], char *str)
 {
 	while (*str)
 	{
 		buffer[i++] = *str++;
+		len++;
 	}
 }
 
@@ -17,5 +20,15 @@ void add_to_buffer_char(char buffer[], char c)
 	if (c)
 	{
 		buffer[i++] = c;
+		len++;
 	}
+}
+
+int print_buffer(char buffer[])
+{
+	int x = len;
+	write(1, buffer, len);
+	i = 0;
+	len = 0;
+	return(x);
 }
